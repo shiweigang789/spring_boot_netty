01e0d8e1a74065e58b473619f4efa91dd3d0fc33
shiweigang <shiweigang@akey.me>
2023-03-29 13:10:36 +0800

    Spring Boot Netty

---
+++ src/main/java/com/example/demo/web/IndexController.java
@@ -0,0 +1,36 @@
+package com.example.demo.web;
+
+import com.example.demo.server.NettyServer;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+
+@RestController
+//@RequestMapping(value = "/nettyserver", method = RequestMethod.GET)
+public class IndexController {
+
+    @RequestMapping(value = "/say")
+    @ResponseBody
+    public String say(String msg) {
+        return "hi " + msg;
+    }
+
+    @Resource
+    private NettyServer nettyServer;
+
+    @RequestMapping(value = "/localAddress")
+    @ResponseBody
+    public String localAddress() {
+        return "nettyServer localAddress " + nettyServer.getChannel().localAddress();
+    }
+
+    @RequestMapping(value = "/isOpen")
+    @ResponseBody
+    public String isOpen() {
+        return "nettyServer isOpen " + nettyServer.getChannel().isOpen();
+    }
+
+}
